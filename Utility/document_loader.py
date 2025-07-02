f80b73aa9722ec3cc97cb112e4ac001592bf7685
Sonal Raj <b22me062@iitj.ac.in>
2025-07-03 01:07:41 +0530

    document loader code

---
+++ Utility/document_loader.py
@@ -0,0 +1,13 @@
+from langchain.document_loaders import PyPDFLoader
+
+def documents_loader(path: str):
+    """
+    Load documents from a given path.
+    Args:
+        path (str): The path to the document.
+    Returns:
+        list: A list of loaded documents.
+    """
+    loader = PyPDFLoader(path)
+    documents = loader.load()
+    return documents
