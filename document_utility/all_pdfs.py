7307e36306b612b478acd70d4213967412607b23
Sonal Raj <b22me062@iitj.ac.in>
2025-07-07 21:04:27 +0530

    code for loading all pdf

--- document_utility/all_pdfs.py
+++ document_utility/all_pdfs.py
@@ -3,4 +3,5 @@ import os
 def list_pdfs(folder="Data/PDF_Files"):
     """List all PDF files in the specified folder."""
     all_files = os.listdir(folder)
+    all_files = [file.split('.')[0] for file in all_files]
     return all_files if all_files else ["No PDF files found"]
